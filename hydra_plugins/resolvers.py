68fc9bac42eef4063e031c438dd3335fa6373065
Guillaume Martres <smarter@ubuntu.com>
2025-12-27 23:22:25 +0100

    Add a json serializer to avoid string values

--- hydra_plugins/resolvers.py
+++ hydra_plugins/resolvers.py
@@ -1,5 +1,6 @@
 """Custom OmegaConf resolvers for DVC Hydra composition."""
 
+import json
 from typing import Any, Optional
 
 from omegaconf import OmegaConf
@@ -24,7 +25,15 @@ def _if_set(value: Optional[Any], flag: str) -> str:
     return ""
 
 
+def _json(value: Any) -> str:
+    """Serialize value to JSON string."""
+    if OmegaConf.is_config(value):
+        value = OmegaConf.to_container(value, resolve=True)
+    return json.dumps(value)
+
+
 # Register resolvers
 OmegaConf.register_new_resolver("if", _if_else, replace=True)
 OmegaConf.register_new_resolver("if_not_empty", _if_not_empty, replace=True)
 OmegaConf.register_new_resolver("if_set", _if_set, replace=True)
+OmegaConf.register_new_resolver("json", _json, replace=True)
